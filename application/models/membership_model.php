ff17fe6bec5a3eff34bafd3b92f2ec71f4760e55
yanhanry <wlgcwcl@163.com>
2014-07-04 23:09:36 +0800

    ipp
    ipp

--- application/models/membership_model.php
+++ application/models/membership_model.php
@@ -19,21 +19,22 @@ class Membership_model extends CI_Model{
 		$new_member_insert_data= array(
 			'email_address' => $this->input->post('email_address'),
 			'username' => $this->input->post('username'),
-			'password' => md5($this->input->post('password'))
+			'password' => md5($this->input->post('password')),
+			'power' => $this->getpower()
 		);
 
 		$insert = $this->db->insert('membership',$new_member_insert_data);
 		return $insert;
 	}
 
-	function setpower()
+	function getpower()
 	{
 		$power = 0;
 		for($i=1;$i<11;$i++){
-			$power += $this->input->post('q'+$i);
+			$power += intval($this->input->post('q'.$i));
 		}
-		//$power=$this->input->post('q1')+post('q2')+post('q3')+post('q4')+post('q5')+post('q6')+post('q7')+post('q8')+post('q9')+post('q10');
-		$insert= $this->db->inseet('membership',$);
-		return $insert;
+		var_dump($power);
+		var_dump($this->input->post());
+		return $power;
 	}
 }
